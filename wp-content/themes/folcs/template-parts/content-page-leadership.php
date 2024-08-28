8739611d35a0ad0273f84897c14b3293064b3c10
EllisDesign <bill@ellisdesign.com>
2024-08-27 17:51:46 -0700

    memoriam

--- wp-content/themes/folcs/template-parts/content-page-leadership.php
+++ wp-content/themes/folcs/template-parts/content-page-leadership.php
@@ -14,7 +14,7 @@
 
 <div class="app">
 
-<section class="h1-margin-30 sequence-margin-first-abbreviated sequence-margin-last section-bottom-border">
+<section class="h1-margin-30 sequence-margin-first-abbreviated section-margin-last section-bottom-border">
 	<div class="column-limit">
 		<div class="type-limit">
 			<div class="type-center sequence-margin-last">
@@ -60,6 +60,7 @@
 
 	</div>
 </section>
+
 
 <section class="h1-margin-30 section-margin-first-border section-margin-last">
 	<div class="column-limit">
@@ -107,6 +108,63 @@
 
 	</div>
 </section>
+
+<?php
+	
+	$memoriam = get_field('memoriam_items');
+
+	if( $memoriam ):
+?>
+
+<section class="h1-margin-30 section-margin-first-border section-margin-last section-top-border">
+	<div class="column-limit">
+		<div class="type-limit">
+			<div class="type-center sequence-margin-last">
+				<?php the_field('third_details'); ?>
+			</div>
+		</div>
+
+		<div class="row event-cards p-margin-5 leadership-portraits">
+
+			<?php
+
+				$posts = get_field('memoriam_items');
+
+				if( $posts ):
+
+				foreach( $posts as $post):
+				setup_postdata($post);
+
+				$leader = get_field('leadership_item');
+			?>
+
+				<div class="col-lg-3 col-md-6 col-sm-12 event-card">
+					<div class="event-items">
+						<div class="event-item js-bio-portrait" data-portrait="<?php echo $leader['sizes']['medium']; ?>" style="background-image: url('<?php echo $leader['sizes']['thumbnail_cropped']; ?>')"></div>
+						<div class="event-details js-bio-details" data-name="<?php the_field('leadership_name'); ?>" data-title="<?php the_field('leadership_title'); ?>">
+							<p><b><?php the_field('leadership_name'); ?></b></p>
+							<p><?php the_field('leadership_title'); ?></p>
+						</div>
+						<a href="" class="js-bio"></a>
+						<div class="leadership-bio-data"><?php the_field('leadership_details'); ?></div>
+					</div>
+				</div>
+
+			<?php
+				endforeach;
+				wp_reset_postdata();
+
+				endif;
+			?>
+
+		</div>
+
+	</div>
+</section>
+
+<?php
+	endif;
+?>
 
 
 <section class="leadership-bio p-margin-15 js-bio-detail">
