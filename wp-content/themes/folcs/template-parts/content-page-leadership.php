a464d95b7e502b339569de4ffa5980063c8e6ac9
EllisDesign <bill@ellisdesign.com>
2025-09-03 12:37:50 -0700

    leadership

--- wp-content/themes/folcs/template-parts/content-page-leadership.php
+++ wp-content/themes/folcs/template-parts/content-page-leadership.php
@@ -62,6 +62,54 @@
 </section>
 
 
+<section class="h1-margin-30 section-margin-first-border section-margin-last section-bottom-border">
+	<div class="column-limit">
+		<div class="type-limit">
+			<div class="type-center sequence-margin-last">
+				<?php echo get_field('media_details'); ?>
+			</div>
+		</div>
+
+		<div class="row event-cards p-margin-5 leadership-portraits">
+
+			<?php
+				
+				$posts = get_field('media_items');
+
+				if( $posts ):
+
+					foreach( $posts as $post):
+					setup_postdata($post);
+
+					$leader = get_field('leadership_item');
+				?>
+
+				<div class="col-lg-3 col-md-6 col-sm-12 event-card">
+					<div class="event-items">
+						<div class="event-item js-bio-portrait" data-portrait="<?php echo $leader['sizes']['medium']; ?>" style="background-image: url('<?php echo $leader['sizes']['thumbnail_cropped']; ?>')"></div>
+						<div class="event-details js-bio-details" data-name="<?php echo get_field('leadership_name'); ?>" data-title="<?php echo get_field('leadership_title'); ?>">
+							<p><b><?php echo get_field('leadership_name'); ?></b></p>
+							<p><?php echo get_field('leadership_title'); ?></p>
+						</div>
+						<a href="" class="js-bio"></a>
+						<div class="leadership-bio-data"><?php echo get_field('leadership_details'); ?></div>
+					</div>
+				</div>
+
+			<?php
+					endforeach;
+					wp_reset_postdata(); 
+
+				endif; 
+
+			?>
+
+		</div>
+
+	</div>
+</section>
+
+
 <section class="h1-margin-30 section-margin-first-border section-margin-last">
 	<div class="column-limit">
 		<div class="type-limit">
